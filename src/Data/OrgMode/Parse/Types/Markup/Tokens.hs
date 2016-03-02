c27a3b6a79e14f12c97e05897dc670074e49f32a
Scott Murphy <scottmurphy09@gmail.com>
2016-03-02 17:46:42 -0600

    PostTokens defined

--- src/Data/OrgMode/Parse/Types/Markup/Tokens.hs
+++ src/Data/OrgMode/Parse/Types/Markup/Tokens.hs
@@ -1,15 +1,36 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NoImplicitPrelude          #-}
 {-# LANGUAGE OverloadedStrings          #-}
-module OrgMode.Parse.Types.Markup.Tokens (PreToken) where
+
+
+{-|
+
+Tokens are nice to specifically design so that pretty printing can be generated programmatically at times
+
+|-}
+module OrgMode.Parse.Types.Markup.Tokens ( PreToken,makePreToken
+                                         , PostToken, makePostToken) where
 import           Data.Monoid ((<>))
 import           Data.String
 import           Data.Text
 import           Prelude     (Bool (..), Char, Show, const, error, maybe, show,
                               ($), (++), (.), (==))
 
--- |   PRE is a whitespace character, `(', `{' or a double quote.
--- I took out ~’~ for now
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- |   PRE is a whitespace character, `(', `{' ~’~ or a double quote.
 -- IsString is defined so you can write tokens with literals
 newtype PreToken = PreToken Text
   deriving (Show)
@@ -21,11 +42,73 @@ makePreToken c = if isMatch
   where
     isMatch = any (== c) tokenList
     generateError = error ((show c) ++ " is not a valid PRE token, must be whitespace or " ++ ((unpack . intersperse ' ') tokenList))
-    tokenList = " ({\""
+    tokenList = " ({\"’" <> whiteSpaceCharacters
 
 
 instance IsString PreToken where
   fromString [] = error "no pre token given"
   fromString (c:[]) = makePreToken c
   fromString _ = error "only one character should be a pre"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- |   POST is a whitespace character, `-', `.', ~,~, `:', `!', `?', ~’~,
+--   `)', `}' or a double quote.
+
+newtype PostToken = PostToken Text
+  deriving (Show)
+
+makePostToken :: Char -> PostToken
+makePostToken c = if isMatch
+                     then PostToken (cons c empty)
+                     else generateError
+  where
+    isMatch = any (== c) tokenList
+    generateError = error ((show c) ++ " is not a valid POST token, must be whitespace or " ++ ((unpack . intersperse ' ') tokenList))
+    tokenList = "-.,:!?’\"" <> whiteSpaceCharacters
+
+
+
+
+
+instance IsString PostToken where
+  fromString [] = error "no post token given"
+  fromString (c:[]) = makePostToken c
+  fromString _ = error "only one character should be a post token"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- | This is a list of all valid whitespace tokens.
+whiteSpaceCharacters = "\n\r \t"
+
+
+
 
